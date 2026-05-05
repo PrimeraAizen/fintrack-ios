b71498f0998105ad0c4cf00c70a87dc3e99916ba
PrimeraAizen <diyas.nurullaev@gmail.com>
2026-05-05 19:55:42 +0500

    Initial Commit

---
+++ FinTrackIOS/FinTrackIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinTrackIOSApp.swift
+//  FinTrackIOS
+//
+//  Created by Дияс Нуруллаев on 09.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct FinTrackIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FinTrackIOSTests/FinTrackIOSTests.swift
@@ -0,0 +1,19 @@
+//
+//  FinTrackIOSTests.swift
+//  FinTrackIOSTests
+//
+//  Created by Дияс Нуруллаев on 09.05.2026.
+//
+
+import Testing
+@testable import FinTrackIOS
+
+struct FinTrackIOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
